ab3e721cb0cc6f3bb591d2d1901310fa8c454cef
Miguel Gonzalez <sb.mgonzalez1@gmail.com>
2017-12-19 18:04:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    set :database, 'sqlite3:db/create_post.db'
     set :public_folder, 'public'
     set :views, 'app/views'
   end
